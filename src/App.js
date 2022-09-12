e19e5e33f064a1c44f0529d07cf7f0f5843829e1
jelenaa25 <beganovicjelena@gmail.com>
2022-09-12 23:28:13 +0200

    dodata dela u kolekciju

--- src/App.js
+++ src/App.js
@@ -19,27 +19,27 @@ function App() {
   const [dela] = useState([
     {
       id:1,
-      naziv:"UMETNICA",
+      naziv:"BljESTAVOST",
       opis:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      slika:"https://www.serbianaart.rs/images/44650.jpg",
+      slika:"https://www.serbianaart.rs/images/bljestavost-000000-s57624.jpg",
       cena:150000,
-      autor:"Stojkovic Marija"
+      autor:"Budimir Branka"
     },
     {
       id:2,
       naziv:"BROD",
       opis:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      slika:"https://www.serbianaart.rs/images/44650.jpg",
+      slika:"https://www.serbianaart.rs/images/tisina-000000-s57589.jpg",
       cena:69999,
       autor:"Stojkovic Marija"
     },
     {
       id:3,
-      naziv:"ZENA",
+      naziv:"TRI BELA GOLUBA",
       opis:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-      slika:"https://www.serbianaart.rs/images/44650.jpg",
+      slika:"https://www.serbianaart.rs/images/tri-bela-goluba-141013-s57586.jpg",
       cena:8000,
-      autor:"Stojkovic Marija"
+      autor:"Snezana Djurkovic"
     },
     {
       id:4,
@@ -48,6 +48,38 @@ function App() {
       slika:"https://www.serbianaart.rs/images/44650.jpg",
       cena:15000,
       autor:"Stojkovic Marija"
+    },
+    {
+      id:5,
+      naziv:"STARI GRAD",
+      opis:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      slika:"https://www.serbianaart.rs/images/stari-grad-083849-s57591.jpg",
+      cena:150000,
+      autor:"Budimir Branka"
+    },
+    {
+      id:6,
+      naziv:"ZLATNA PRASINA",
+      opis:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      slika:"https://www.serbianaart.rs/images/zlatna-prasina-054813-s57548.jpg",
+      cena: 7500,
+      autor:"Stojkovic Marija"
+    },
+    {
+      id:7,
+      naziv:"U STAKLENOJ BASTI",
+      opis:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      slika:"https://www.serbianaart.rs/images/u-stakenoj-basti-080919-s57575.jpg",
+      cena: 18000,
+      autor:"Veselinovic Slobodan"
+    },
+    {
+      id:8,
+      naziv:"KOSOVKA DEVOJKA",
+      opis:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      slika:"https://www.serbianaart.rs/images/kosovka-devojka-000000-s57572.jpg",
+      cena: 24000,
+      autor:"Ana Art"
     }
   ]);
   const [filtriranaDela,setFiltriranaDela]= useState(dela);
