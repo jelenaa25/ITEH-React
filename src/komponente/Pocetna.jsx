302e5c298233721f1b1944fd93ec1fdc09f0c4d4
jelenaa25 <beganovicjelena@gmail.com>
2022-09-12 21:15:36 +0200

    dodat sadrzaj na pocetnu stranicu

--- src/komponente/Pocetna.jsx
+++ src/komponente/Pocetna.jsx
@@ -5,7 +5,53 @@ function Pocetna() {
     return (
        
          <div className="pocetna">
-            <h1>POCETNA</h1>
+              
+              <section className="firstsection">
+        <div className="box-main">
+            <div className="firstHalf">
+                <h1 className="text-big" id="web">
+                    Dobrodosli u ArtShop!
+                </h1>
+                 
+                <p className="text-small">
+                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque soluta quibusdam nulla fugiat reiciendis possimus vitae nam ea accusantium architecto magnam corrupti, non assumenda omnis, ipsum dolorum rerum molestiae placeat.
+                </p>
+ 
+ 
+            </div>
+        </div>
+    </section>
+ 
+    <section className="secondsection">
+        <div className="box-main">
+            <div className="secondHalf">
+                <h1 className="text-big" id="program">
+                    Oktrijte najnovije mlade umetnike i njihove radove!
+                </h1>
+                <p className="text-small">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem exercitationem amet laudantium ipsam ullam officiis sed deleniti laborum, sint qui, quas est mollitia, porro nesciunt numquam voluptatibus voluptatum quod commodi.
+                </p>
+ 
+ 
+            </div>
+        </div>
+    </section>
+ 
+    <section className="section">
+        <div className="paras">
+            <h1 className="sectionTag text-big">Najpovoljniji webshop</h1>
+ 
+            <p className="sectionSubTag text-small">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium placeat necessitatibus quae. Sequi, eaque quo? Iusto reiciendis qui repellendus sint non. Veniam repellat eos ullam accusamus similique sunt, omnis beatae.
+            </p>
+ 
+ 
+        </div>
+ 
+        <div className="thumbnail">
+            <img src="https://media.istockphoto.com/photos/used-brushes-on-an-artists-palette-of-colorful-oil-paint-picture-id636761588?k=20&m=636761588&s=612x612&w=0&h=ew-Gc9JIh5PGLbxaW21a0Y4D_w7d1zsngEPX74e5HDs=" alt="laptop image"/>
+        </div>
+    </section>
 
 
          </div>
